1eafd04b9c81ab0d3dc3a8a8c50970f2f8bf80cd
Weber <andweber92@gmail.com>
2023-07-14 19:46:37 -0300

    fix: updated imports and removing warnings from tests

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,11 @@
-from pydantic import BaseSettings
+import os
+from dotenv import load_dotenv, find_dotenv
+from pydantic_settings import BaseSettings
 
+load_dotenv(find_dotenv())
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "sqlite:///./test.db"
-
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
 
 settings = Settings()
