26bd9efafe118711ca4d7109c5f10de80e3a3893
Weber <andweber92@gmail.com>
2023-07-14 20:01:30 -0300

    feature: added credit card endpoints: create and read

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
+from app.api.v1.endpoints.credit_card import router as credit_card_router
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(credit_card_router, prefix="/api/v1")
