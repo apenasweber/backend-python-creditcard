b3a6f6d8a043977f02499b1cad68ee61d9d0d245
Weber <andweber92@gmail.com>
2023-07-15 11:27:45 -0300

    applied business rules with unit tests validations

--- app/api/v1/endpoints/credit_card.py
+++ app/api/v1/endpoints/credit_card.py
@@ -3,33 +3,52 @@ from sqlalchemy.orm import Session
 from typing import List
 
 from app.core.database import get_db
-from app.models.credit_card import CreditCard as CreditCardModel
-from app.schemas.credit_card import CreditCard, CreditCardCreate
+from app.schemas.credit_card import CreditCardSchema, CreditCardCreateSchema, CreditCardUpdateSchema
 from app.services.crypto_service import CryptoService
+from datetime import datetime
+from creditcard import CreditCard
+from app.models.credit_card import CreditCardModel
 
 router = APIRouter()
 
 
-@router.post("/credit-card", response_model=CreditCard,tags=["Cartões de Crédito"])
+@router.post("/credit-card", response_model=CreditCardSchema, tags=["Cartões de Crédito"])
 def create_credit_card(
-    card: CreditCardCreate,
+    card: CreditCardCreateSchema,
     db: Session = Depends(get_db),
     crypto_service: CryptoService = Depends(),
 ):
+    try:
+        exp_date_str = card.exp_date.strftime("%m/%Y")
+        exp_date = datetime.strptime(exp_date_str, "%m/%Y")
+        if exp_date < datetime.now():
+            raise HTTPException(status_code=400, detail="Data de expiração inválida")
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Data de expiração inválida")
+
+    if not card.holder or len(card.holder) < 3:
+        raise HTTPException(status_code=400, detail="Titular inválido")
+
+    cc = CreditCard(card.number)
+    if not cc.is_valid():
+        raise HTTPException(status_code=400, detail="Número de cartão inválido")
+
     encrypted_number = crypto_service.encrypt(card.number)
+
+    if card.cvv and (len(card.cvv) < 3 or len(card.cvv) > 4):
+        raise HTTPException(status_code=400, detail="CVV inválido")
+
     db_card = CreditCardModel(**card.dict(), number=encrypted_number)
     db.add(db_card)
     db.commit()
     db.refresh(db_card)
     return db_card
 
-
-@router.get("/credit-card", response_model=List[CreditCard],tags=["Cartões de Crédito"])
+@router.get("/credit-card", response_model=List[CreditCardSchema],tags=["Cartões de Crédito"])
 def read_credit_cards(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return db.query(CreditCardModel).offset(skip).limit(limit).all()
 
-
-@router.get("/credit-card/{card_id}", response_model=CreditCard,tags=["Cartões de Crédito"])
+@router.get("/credit-card/{card_id}", response_model=CreditCardSchema,tags=["Cartões de Crédito"])
 def read_credit_card(card_id: int, db: Session = Depends(get_db)):
     card = db.query(CreditCardModel).filter(CreditCardModel.id == card_id).first()
     if card is None:
--- app/schemas/credit_card.py
+++ app/schemas/credit_card.py
@@ -4,19 +4,25 @@ from typing import Optional
 from pydantic import BaseModel
 
 
-class CreditCardBase(BaseModel):
+class CreditCardBaseSchema(BaseModel):
     exp_date: date
     holder: str
+    number: str
     cvv: Optional[str] = None
 
 
-class CreditCardCreate(CreditCardBase):
+class CreditCardCreateSchema(CreditCardBaseSchema):
     number: str
 
 
-class CreditCard(CreditCardBase):
+class CreditCardSchema(CreditCardBaseSchema):
     id: int
-    number: str  # this will be the encrypted number
+    number: str  
 
     class Config:
         orm_mode = True
+
+class CreditCardUpdateSchema(BaseModel):
+    exp_date: str
+    holder: str
+    cvv: str
