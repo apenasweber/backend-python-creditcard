b20809c0a9648df255d5d24570e57db23056fb03
Weber <andweber92@gmail.com>
2023-07-15 22:07:20 -0300

    fix: remove cryptography service

--- tests/test_crypto_service.py
+++
@@ -1,8 +0,0 @@
-from app.services.crypto_service import CryptoService
-
-
-def test_crypto_service():
-    crypto_service = CryptoService()
-    encrypted_text = crypto_service.encrypt("plaintext")
-    assert encrypted_text != "plaintext"
-    assert crypto_service.decrypt(encrypted_text) == "plaintext"
