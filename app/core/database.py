c173a457f131d78baee5ae568f911373f6a5ff9b
Weber <andweber92@gmail.com>
2023-07-16 21:13:34 -0300

    chore: change import to avoid warnings on test stdout

--- app/core/database.py
+++ app/core/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from app.core.settings import settings
 
 engine = create_engine(settings.DATABASE_URL)
